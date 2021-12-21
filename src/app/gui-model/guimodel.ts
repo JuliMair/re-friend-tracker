5f177b682db4ec4ead1c2fbb3c78c2bac1b1602e
JuliMair <mairjul@students.zhaw.ch>
2021-12-21 10:29:42 +0000

    Änderungen gemäss Uebung 12

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Julian Mair Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
